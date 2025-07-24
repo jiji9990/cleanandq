2d6d51317f1f38eea1da70b58e556a02cad693c8
jiji9990 <junyoung200252@gmail.com>
2025-07-24 14:58:47 +0900

    Fix Netlify Functions paths

--- app/netlify/functions/server.js
+++ app/netlify/functions/server.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const app = require('../../server');
+const path = require('path');
+
+// Create Express app
+const app = express();
+
+// routing
+const home = require('../../src/routes/home');
+
+// app, setting
+app.set("views", path.join(__dirname, "../../src/views"));
+app.set("view engine", "ejs");
+
+// use -> method to register middleware
+app.use(express.static(path.join(__dirname, "../../src/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", home);
+
+// robots.txt
+app.get("/robots.txt", (req, res) => {
+  res.type("text/plain");
+  res.send("User-agent: *\nAllow:/");
+});
 
 // Export the serverless function
 exports.handler = serverless(app);
