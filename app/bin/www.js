fe9003352b36d4c6ef0178c2491210b6f06710f8
jiji9990 <junyoung200252@gmail.com>
2025-07-24 14:37:10 +0900

    Fix vercel deployment: port config and build settings

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const app = require('../server');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
